f40eab23776f3c7072a21825de1eee3f134fbaf0
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-05-17 19:25:05 -0700

    nicer tests

--- test/test.js
+++ test/test.js
@@ -13,44 +13,48 @@ function sinSamples (hz, samples, sampleRate, data) {
   }
 }
 
-function encode (msg, data) {
+function writeEncodedMessage (msg, opts, data) {
   msg.forEach(function (ch) {
     ch = ch.charCodeAt(0)
     for (var i=0; i < 8; i++) {
       var bit = ch & 0x1
       ch >>= 1
-      sinSamples(bit === 0 ? space : mark, sampleRate / baud, sampleRate, data)
+      sinSamples(bit === 0 ? opts.space : opts.mark,
+          opts.sampleRate / opts.baud, opts.sampleRate, data)
     }
   })
 }
 
+function writePreamble (opts, data) {
+  // preamble space (>= 1 baud (1 sampleRate))
+  sinSamples(opts.space, opts.sampleRate / opts.baud, opts.sampleRate, data)
+
+  // begin space symbol (== 1 baud (1 sampleRate))
+  sinSamples(opts.mark, opts.sampleRate / opts.baud, opts.sampleRate, data)
+}
+
 test('basic', function (t) {
-  var sampleRate = 8000
-  var samplesPerFrame = 100
-  var space = 324
-  var mark = 884
-  var baud = 1
+  var opts = {
+    sampleRate: 8000,
+    samplesPerFrame: 100,
+    space: 324,
+    mark: 884,
+    baud: 1
+  }
 
   var msg = 'hello world!'.split('')
   var data = []
 
-  // make pre-amble longer
-  // sinSamples(space, 30, data)
+  writePreamble(opts, data)
 
-  // preamble space (>= 1 baud (1 sampleRate))
-  sinSamples(space, sampleRate / baud, sampleRate, data)
-
-  // begin space symbol (== 1 baud (1 sampleRate))
-  sinSamples(mark, sampleRate / baud, sampleRate, data)
-
-  encode(msg, data)
+  writeEncodedMessage(msg, opts, data)
 
   var decoder = new Decoder({
-    mark: mark,
-    space: space,
-    baud: baud,
-    sampleRate: sampleRate,
-    samplesPerFrame: samplesPerFrame
+    mark: opts.mark,
+    space: opts.space,
+    baud: opts.baud,
+    sampleRate: opts.sampleRate,
+    samplesPerFrame: opts.samplesPerFrame
   })
 
   decoder.pipe(concat(function (data) {
@@ -60,8 +64,87 @@ test('basic', function (t) {
 
   var i = 0
   while (i < data.length) {
-    var frame = data.slice(i, i + samplesPerFrame)
-    i += samplesPerFrame
+    var frame = data.slice(i, i + opts.samplesPerFrame)
+    i += opts.samplesPerFrame
+    decoder.write(frame)
+  }
+  decoder.end()
+})
+
+test('extra long preamble', function (t) {
+  var opts = {
+    sampleRate: 8000,
+    samplesPerFrame: 100,
+    space: 324,
+    mark: 884,
+    baud: 1
+  }
+
+  var msg = 'hello world!'.split('')
+  var data = []
+
+  // make pre-amble longer
+  sinSamples(opts.space, 30, opts.sampleRate, data)
+
+  writePreamble(opts, data)
+
+  writeEncodedMessage(msg, opts, data)
+
+  var decoder = new Decoder({
+    mark: opts.mark,
+    space: opts.space,
+    baud: opts.baud,
+    sampleRate: opts.sampleRate,
+    samplesPerFrame: opts.samplesPerFrame
+  })
+
+  decoder.pipe(concat(function (data) {
+    t.equal(data.toString(), 'hello world!')
+    t.end()
+  }))
+
+  var i = 0
+  while (i < data.length) {
+    var frame = data.slice(i, i + opts.samplesPerFrame)
+    i += opts.samplesPerFrame
+    decoder.write(frame)
+  }
+  decoder.end()
+})
+
+test('high frequency', function (t) {
+  var opts = {
+    sampleRate: 44100,
+    samplesPerFrame: 100,
+    space: 324,
+    mark: 884,
+    baud: 1
+  }
+
+  var msg = 'hello world!'.split('')
+  var data = []
+
+  writePreamble(opts, data)
+
+  writeEncodedMessage(msg, opts, data)
+
+  var decoder = new Decoder({
+    mark: opts.mark,
+    space: opts.space,
+    baud: opts.baud,
+    sampleRate: opts.sampleRate,
+    samplesPerFrame: opts.samplesPerFrame
+  })
+
+  decoder.pipe(concat(function (data) {
+    t.equal(data.toString(), 'hello world!')
+    t.end()
+  }))
+
+  var i = 0
+  while (i < data.length) {
+    var frame = data.slice(i, i + opts.samplesPerFrame)
+    i += opts.samplesPerFrame
     decoder.write(frame)
   }
   decoder.end()
